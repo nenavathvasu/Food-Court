2531227bb5e8acf0512eb96053883988ae0e06db
nenavathvasu <vasunenavath11@gmail.com>
2026-02-25 01:13:03 +0530

    first commit

--- src/api/publicApi.js
+++ src/api/publicApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const publicApi = axios.create({
-  baseURL: "https://backend-express-nu.vercel.app/",
+  baseURL: "https://backend-express-nu.vercel.app/api/v1",
   headers: {
     "Content-Type": "application/json",
   },
